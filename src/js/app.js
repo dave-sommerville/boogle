1fb773ee005137c257a662b839ddc3f6096037fc
Dave Sommerville <dave.r.sommerville@outlook.com>
2025-03-07 19:53:34 -0600

    Dice arrays created

---
+++ src/js/app.js
@@ -0,0 +1,52 @@
+'use strict';
+//Boogle regular 
+let Dice1 =	[A, A, C, I, O, T];
+let Dice2 =	[A, H, M, O, R, S];
+let Dice3 =	[E, G, K, L, U, Y];
+let Dice4 =	[A, B, I, L, T, Y];
+let Dice5 =	[A, C, D, E, M, P];
+let Dice6 =	[E, G, I, N, T, V];
+let Dice7 =	[G, I, L, R, U, W];
+let Dice8	=	[E, L, P, S, T, U];
+let Dice9	=	[D, E, N, O, S, W];
+let Dice10	=	[A, C, E, L, R, S];
+let Dice11	=	[A, B, J, M, O, Q];
+let Dice12	=	[E, E, F, H, I, Y];
+let Dice13	=	[E, H, I, N, P, S];
+let Dice14	=	[D, K, N, O, T, U];
+let Dice15	=	[A, D, E, N, V, Z];
+let Dice16	=	[B, I, F, O, R, X];
+
+//The Big Boog 
+let BigDice1	=	[A, A, A, F, R, S];
+let BigDice2	=	[A, A, E, E, E, E];
+let BigDice3	=	[A, A, F, I, R, S];
+let BigDice4	=	[A, D, E, N, N, N];
+let BigDice5	=	[A, E, E, E, E, M];
+let BigDice6	=	[A, E, E, G, M, U];
+let BigDice7	=	[A, E, G, M, N, N];
+let BigDice8	=	[A, F, I, R, S, Y];
+let BigDice9	=	[B, J, K, Q, X, Z];
+let BigDice10	=	[C, C, E, N, S, T];
+let BigDice11	=	[C, E, I, I, L, T];
+let BigDice12	=	[C, E, I, L, P, T];
+let BigDice13	=	[C, E, I, P, S, T];
+let BigDice14	=	[D, D, H, N, O, T];
+let BigDice15	=	[D, H, H, L, O, R];
+let BigDice16	=	[D, H, L, N, O, R];
+let BigDice17	=	[E, I, I, I, T, T];
+let BigDice18	=	[E, M, O, T, T, T];
+let BigDice19	=	[E, N, S, S, S, U];
+let BigDice20	=	[F, I, P, R, S, Y];
+let BigDice21	=	[G, O, R, R, V, W];
+let BigDice22	=	[I, P, R, S, T, T];
+let BigDice23	=	[N, O, O, T, U, W];
+let BigDice24	=	[O, O, O, T, T, U];
+let BigDice25	=	[C, E, I, I, T, T];
+
+function rollTheDice() {
+
+}
+function moveTheDice() {
+
+}
