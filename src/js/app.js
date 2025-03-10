ce60925318b6f5c5f932c4ff1cf6025ac67d2849
Dave Sommerville <dave.r.sommerville@outlook.com>
2025-03-10 13:12:24 -0500

    Boogle made active, interface and styling remaining

--- src/js/app.js
+++ src/js/app.js
@@ -1,48 +1,51 @@
 'use strict';
+function select(selector, scope = document) {
+  return scope.querySelector(selector);
+}
 //Boogle regular 
-let Dice1 =	[A, A, C, I, O, T];
-let Dice2 =	[A, H, M, O, R, S];
-let Dice3 =	[E, G, K, L, U, Y];
-let Dice4 =	[A, B, I, L, T, Y];
-let Dice5 =	[A, C, D, E, M, P];
-let Dice6 =	[E, G, I, N, T, V];
-let Dice7 =	[G, I, L, R, U, W];
-let Dice8	=	[E, L, P, S, T, U];
-let Dice9	=	[D, E, N, O, S, W];
-let Dice10	=	[A, C, E, L, R, S];
-let Dice11	=	[A, B, J, M, O, Q];
-let Dice12	=	[E, E, F, H, I, Y];
-let Dice13	=	[E, H, I, N, P, S];
-let Dice14	=	[D, K, N, O, T, U];
-let Dice15	=	[A, D, E, N, V, Z];
-let Dice16	=	[B, I, F, O, R, X];
+let Dice1 = ['A', 'A', 'C', 'I', 'O', 'T'];
+let Dice2 = ['A', 'H', 'M', 'O', 'R', 'S'];
+let Dice3 = ['E', 'G', 'K', 'L', 'U', 'Y'];
+let Dice4 = ['A', 'B', 'I', 'L', 'T', 'Y'];
+let Dice5 = ['A', 'C', 'D', 'E', 'M', 'P'];
+let Dice6 = ['E', 'G', 'I', 'N', 'T', 'V'];
+let Dice7 = ['G', 'I', 'L', 'R', 'U', 'W'];
+let Dice8 = ['E', 'L', 'P', 'S', 'T', 'U'];
+let Dice9 = ['D', 'E', 'N', 'O', 'S', 'W'];
+let Dice10 = ['A', 'C', 'E', 'L', 'R', 'S'];
+let Dice11 = ['A', 'B', 'J', 'M', 'O', 'Q'];
+let Dice12 = ['E', 'E', 'F', 'H', 'I', 'Y'];
+let Dice13 = ['E', 'H', 'I', 'N', 'P', 'S'];
+let Dice14 = ['D', 'K', 'N', 'O', 'T', 'U'];
+let Dice15 = ['A', 'D', 'E', 'N', 'V', 'Z'];
+let Dice16 = ['B', 'I', 'F', 'O', 'R', 'X'];
 
 //The Big Boog 
-let BigDice1	=	[A, A, A, F, R, S];
-let BigDice2	=	[A, A, E, E, E, E];
-let BigDice3	=	[A, A, F, I, R, S];
-let BigDice4	=	[A, D, E, N, N, N];
-let BigDice5	=	[A, E, E, E, E, M];
-let BigDice6	=	[A, E, E, G, M, U];
-let BigDice7	=	[A, E, G, M, N, N];
-let BigDice8	=	[A, F, I, R, S, Y];
-let BigDice9	=	[B, J, K, Q, X, Z];
-let BigDice10	=	[C, C, E, N, S, T];
-let BigDice11	=	[C, E, I, I, L, T];
-let BigDice12	=	[C, E, I, L, P, T];
-let BigDice13	=	[C, E, I, P, S, T];
-let BigDice14	=	[D, D, H, N, O, T];
-let BigDice15	=	[D, H, H, L, O, R];
-let BigDice16	=	[D, H, L, N, O, R];
-let BigDice17	=	[E, I, I, I, T, T];
-let BigDice18	=	[E, M, O, T, T, T];
-let BigDice19	=	[E, N, S, S, S, U];
-let BigDice20	=	[F, I, P, R, S, Y];
-let BigDice21	=	[G, O, R, R, V, W];
-let BigDice22	=	[I, P, R, S, T, T];
-let BigDice23	=	[N, O, O, T, U, W];
-let BigDice24	=	[O, O, O, T, T, U];
-let BigDice25	=	[C, E, I, I, T, T];
+let BigDice1 = ['A', 'A', 'A', 'F', 'R', 'S'];
+let BigDice2 = ['A', 'A', 'E', 'E', 'E', 'E'];
+let BigDice3 = ['A', 'A', 'F', 'I', 'R', 'S'];
+let BigDice4 = ['A', 'D', 'E', 'N', 'N', 'N'];
+let BigDice5 = ['A', 'E', 'E', 'E', 'E', 'M'];
+let BigDice6 = ['A', 'E', 'E', 'G', 'M', 'U'];
+let BigDice7 = ['A', 'E', 'G', 'M', 'N', 'N'];
+let BigDice8 = ['A', 'F', 'I', 'R', 'S', 'Y'];
+let BigDice9 = ['B', 'J', 'K', 'Q', 'X', 'Z'];
+let BigDice10 = ['C', 'C', 'E', 'N', 'S', 'T'];
+let BigDice11 = ['C', 'E', 'I', 'I', 'L', 'T'];
+let BigDice12 = ['C', 'E', 'I', 'L', 'P', 'T'];
+let BigDice13 = ['C', 'E', 'I', 'P', 'S', 'T'];
+let BigDice14 = ['D', 'D', 'H', 'N', 'O', 'T'];
+let BigDice15 = ['D', 'H', 'H', 'L', 'O', 'R'];
+let BigDice16 = ['D', 'H', 'L', 'N', 'O', 'R'];
+let BigDice17 = ['E', 'I', 'I', 'I', 'T', 'T'];
+let BigDice18 = ['E', 'M', 'O', 'T', 'T', 'T'];
+let BigDice19 = ['E', 'N', 'S', 'S', 'S', 'U'];
+let BigDice20 = ['F', 'I', 'P', 'R', 'S', 'Y'];
+let BigDice21 = ['G', 'O', 'R', 'R', 'V', 'W'];
+let BigDice22 = ['I', 'P', 'R', 'S', 'T', 'T'];
+let BigDice23 = ['N', 'O', 'O', 'T', 'U', 'W'];
+let BigDice24 = ['O', 'O', 'O', 'T', 'T', 'U'];
+let BigDice25 = ['C', 'E', 'I', 'I', 'T', 'T'];
 
 const REGULAR_BOOG = [
   Dice1, Dice2, 
@@ -66,14 +69,13 @@ const BIG_BOOG = [
   BigDice15, BigDice16,
   BigDice17, BigDice18,
   BigDice19, BigDice20,
-  BigDice21, bigDice22,
+  BigDice21, BigDice22,
   BigDice23, BigDice24,
   BigDice25
 ];
+const GameBoard = select(".letter-display");
 
-function select(selector, scope = document) {
-  return scope.querySelector(selector);
-}
+
 
 function selectAll(selector, scope = document) {
   return scope.querySelectorAll(selector);
@@ -89,8 +91,8 @@ function create(element) {
   return newElement;
 }
 function shuffle(diceArr) {
-  const shuffledArr = [...deck]; 
-  for (let i = shuffledDeck.length - 1; i > 0; i--) {
+  const shuffledArr = [...diceArr]; 
+  for (let i = shuffledArr.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [shuffledArr[i], shuffledArr[j]] = [shuffledArr[j], shuffledArr[i]]; 
   }
@@ -101,12 +103,10 @@ function shuffleTheBoard(diceArr) {
   let shuffledBoard = shuffle(diceArr);
   shuffledBoard.forEach(cell => {
     const p = create("p");
-    p.textContent = rollTheDice(cell);
+    p.textContent = getRandomItem(cell);
     p.classList.add("grid-item");
-    grid.appendChild(p);
+    GameBoard.appendChild(p);
 });
 }
 
-function rollTheDice(dieArr) {
-  getRandomItem(dieArr);
-}
+shuffleTheBoard(REGULAR_BOOG);
